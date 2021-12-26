bb410fe76e5040e11f3607d3e361f6ac724862b0
赵文浩 <1050986935@qq.com>
2021-12-26 14:05:57 +0800

    添加项目文件。

---
+++ kk.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num1 = 10;
+
+	short num2 = 10;
+
+	long num3 = 10;
+
+	long long num4 = 10;
+
+
+	cout << "num1 = " << num1 << endl;
+	cout << "num2 = " << num2 << endl;
+	cout << "num3 = " << num3 << endl;
+	cout << "num4 = " << num4 << endl;
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;  
+}
